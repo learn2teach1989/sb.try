daebbdf88b6429549d291963e9bf330177d35d42
KARU.SELVABHARATHI <learn2teach1989@gmail.com>
2026-09-09 01:33:19 +0530

    Fix icon paths in PRECACHE_URLS array

--- sw.js
+++ sw.js
@@ -5,8 +5,8 @@ const PRECACHE_URLS = [
   './',
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', function (event) {
